1db006d6a351c8c258241488cf0fd20c842b079c
posluh <nicitak004@yandex.ru>
2024-02-26 22:04:22 +0300

    second commit

--- src/main/java/com/example/demoex/repo/UserRepo.java
+++ src/main/java/com/example/demoex/repo/UserRepo.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsername(String username);
+//    List<User> findByNameContainingIgnoreCase(String name); список пользователь игнорируя регистр
 }
